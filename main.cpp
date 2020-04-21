9fda32e481b01dd0a59acd89b0fe4d85d8ba796e
GeorgeBodea <61265969+GeorgeBodea@users.noreply.github.com>
2020-04-22 01:03:51 +0300

    Am adaugat si fiserul de citire.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "Nod_simplu.h"
 #include "Nod_dublu.h"
 #include "Coada_prioritati.h"
-
+#include <fstream>
 using namespace std;
+ifstream f("date.in");
 
 int main() {
     Coada_prioritati p1;
-    cin >> p1 >> p1 >> p1;
+    f >> p1 >> p1 >> p1;
     Coada_prioritati p2;
     p2 = p1;
 
@@ -19,5 +20,7 @@ int main() {
     cout << p2.top();
     p2.pop();
     cout << p2.top();
+
+    f.close();
     return 0;
 }
